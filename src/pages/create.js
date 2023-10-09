b01caa5a1932560ce98e222adac7c3cb26f4348a
Shahriar Samir <138759491+Shahriar-Samir@users.noreply.github.com>
2023-10-10 01:02:41 +0600

    Update create.js

--- src/pages/create.js
+++ src/pages/create.js
@@ -5,7 +5,7 @@ export const CreateBook = () => {
     <div className='border border-1 border-black p-5 w-60'>
       <Link to="/" className='text-black'> <i class="fa-solid fa-angle-left position-absolute border border-2 border-black ps-4 pe-4 pt-2 pb-2" id="left_btn" style={{ top:"10px",left:"10px",fontSize:"25px"}}></i></Link>
       <h4>Create Book</h4>
-        <form className='d-flex flex-column align-items-center h-100 w-100' method='POST' action='https://book-list-backend-y9vk.onrender.com/books'>
+        <form className='d-flex flex-column align-items-center h-100 w-100' method='POST' action='https://book-list-backend-dadx.onrender.com/books'>
 
          <div className='w-100 d-flex flex-column'>
          <label>Title</label>
